47f41e67a8cf61d96fc47fa5a94ef8f850b49ca4
Antonio <antonioboksic1603@gmail.com>
2023-06-05 16:35:08 +0200

    installato scss, creato componenti e file scss che mi serviranno, eliminato file che non mi serviranno

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,132 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import ListProducts from './components/ListProducts.vue';
+
+
+export default {
+  components: {
+    ListProducts,
+  },
+
+  // copia/incollato array "links da figlio a padre" e cambiato nome in menu per non dover passare il props: " links="links" a riga 75 (funzionerebbe comunque ma così è più chiaro cosa succede)"
+  data() {
+    return {
+      menu: [ 
+        {
+          text:"CHARACHTERS",
+          url:"#",
+          current: false
+      },
+      {
+          text:"COMICS",
+          url:"#",
+          current: true
+      },
+      {
+          text:"MOVIES",
+          url:"#",
+          current: false
+      },
+      {
+          text:"TV",
+          url:"#",
+          current: false
+      },
+      {
+          text:"GAMES",
+          url:"#",
+          current: false
+      },
+      {
+          text:"COLLECTIBLES",
+          url:"#",
+          current: false
+      },
+      {
+          text:"VIDEOS",
+          url:"#",
+          current: false
+      },
+      {
+          text:"FANS",
+          url:"#",
+          current: false
+      },
+      {
+          text:"NEWS",
+          url:"#",
+          current: false
+      },
+      {
+          text:"SHOP",
+          url:"#",
+          current: false
+      },
+
+      ]
+    }
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  
+  <!-- qui passo array "menu" da padre a figlio, array che chiamerò "links" nel componente figlio, perchè li avevo chiamati così in precedenza e quindi non ho bisogno di cambiare nient'altro all'interno del componente figlio -->
+  <AppHeader :links="menu" />
+
+  <main>
+
+
+    <div class="jumbotron">
+      <img src="../public/jumbotron.jpg" alt="">
+    </div>
+
+
+    
+    <ListProducts />
+    
+    
+  </main>
+
+  <AppFooter />
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style lang="scss">
+
+@use './styles/partials/variables.scss' as *;
+@use './styles/general.scss' as *;
+
+
+main {
+  background-color: rgb(30, 30, 30);
+  // height: 100px;
+  padding-bottom: 20px;
+ 
+
+  .container {
+  margin: 0 auto;
+  width: 80%;
+  // border: 1px solid red;
+  text-align: center;
+  
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.jumbotron {
+  height: 300px;
+  
+    img{
+      width: 100%;
+      height: 100%;
+      object-fit:cover;
+      object-position: top;
+      
+    }
+  
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+  
 }
+
+
+
 </style>
+
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,72 @@
+<script>
+// import { ref } from 'vue'
+
+// defineProps({
+//   msg: String,
+// })
+
+// const count = ref(0)
+
+// export default {
+//   name: "AppFooter",
+// }
+
+export default {
+    name: "ProductCard",
+    props: {
+    details: Object
+  }
+}
+
+</script>
+
+<!-- HTML -->
+<template>
+    
+    <!-- blocco prodotto -->
+    <div class="prod-card">
+
+            <div class="img-card">
+                <img :src="details.thumb" :alt="details.series">
+
+            </div>
+        
+            <div class="titolo-card">{{ details.series }}</div>
+
+    </div>
+
+
+</template>
+
+<!-- STILE -->
+
+<style scoped lang="scss">
+
+.prod-card {
+    // border: 1px solid black;
+    width: calc(100%/6 - 20px);
+    margin: 10px;
+
+    height: 150px;
+
+    .img-card{
+        height: 80%;
+
+        img{
+        height: 100%;
+        width: 100%;
+        }
+
+    }
+
+    .titolo-card {
+        padding-top: 6px;
+        height: 20%;
+        text-align: left;
+    }
+
+    
+}
+
+
+</style>
