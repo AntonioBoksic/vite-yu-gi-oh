fce7e82baa25820e05d0c2e45bdc529c2f584e0d
Antonio <antonioboksic1603@gmail.com>
2023-06-05 17:42:33 +0200

    non riesco ad utilizzare Axios installandolo con npm, l'ho dovuto inserire nell'Head dell' index.html e sembra funzionare. Ora mi studio le API e dovrebbe girare in teoria

--- src/App.vue
+++ src/App.vue
@@ -5,89 +5,17 @@ import ListProducts from './components/ListProducts.vue';
 export default {
   components: {
     ListProducts,
-  },
-
-  // copia/incollato array "links da figlio a padre" e cambiato nome in menu per non dover passare il props: " links="links" a riga 75 (funzionerebbe comunque ma così è più chiaro cosa succede)"
-  data() {
-    return {
-      menu: [ 
-        {
-          text:"CHARACHTERS",
-          url:"#",
-          current: false
-      },
-      {
-          text:"COMICS",
-          url:"#",
-          current: true
-      },
-      {
-          text:"MOVIES",
-          url:"#",
-          current: false
-      },
-      {
-          text:"TV",
-          url:"#",
-          current: false
-      },
-      {
-          text:"GAMES",
-          url:"#",
-          current: false
-      },
-      {
-          text:"COLLECTIBLES",
-          url:"#",
-          current: false
-      },
-      {
-          text:"VIDEOS",
-          url:"#",
-          current: false
-      },
-      {
-          text:"FANS",
-          url:"#",
-          current: false
-      },
-      {
-          text:"NEWS",
-          url:"#",
-          current: false
-      },
-      {
-          text:"SHOP",
-          url:"#",
-          current: false
-      },
-
-      ]
-    }
   }
 }
 </script>
 
 <template>
-  
-  <!-- qui passo array "menu" da padre a figlio, array che chiamerò "links" nel componente figlio, perchè li avevo chiamati così in precedenza e quindi non ho bisogno di cambiare nient'altro all'interno del componente figlio -->
-  <AppHeader :links="menu" />
 
   <main>
 
-
-    <div class="jumbotron">
-      <img src="../public/jumbotron.jpg" alt="">
-    </div>
-
-
-    
     <ListProducts />
-    
-    
-  </main>
 
-  <AppFooter />
+  </main>
 
 </template>
 
