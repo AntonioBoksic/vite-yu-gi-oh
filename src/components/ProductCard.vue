5c2ad19ef82391a6f6020781edf871210399dcea
Antonio <antonioboksic1603@gmail.com>
2023-06-05 23:54:57 +0200

    non avevo incluso componente ProductCard nell'ultimo push per sbaglio.

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -33,6 +33,8 @@ export default {
         
             <div class="titolo-card">{{ details.name }}</div>
 
+            <div class="archetype-card">{{ details.archetype }}</div>
+
     </div>
 
 
@@ -43,14 +45,16 @@ export default {
 <style scoped lang="scss">
 
 .prod-card {
-    // border: 1px solid black;
+    border: 1px solid black;
     width: calc(100%/6 - 20px);
-    margin: 10px;
-
-    height: 300px;
+    margin-left: 10px;
+    margin-right: 10px;
+    margin-top: 10px;
+    margin-bottom: 30px;
+    height: 350px;
 
     .img-card{
-        height: 80%;
+        height: 70%;
 
         img{
         height: 100%;
@@ -61,9 +65,18 @@ export default {
 
     .titolo-card {
         padding-top: 6px;
-        height: 20%;
-        text-align: left;
+        height: 15%;
+        text-align: center;
+        font-weight: 900;
     }
+
+    .archetype-card {
+        padding-top: 6px;
+        height: 15%;
+        text-align: center;
+        color: black;
+    }
+
 
     
 }
